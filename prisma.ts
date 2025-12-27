c768e1b09aff1512f860be4ee2248dd3430c502f
Leon Hsieh <leonhsieh@yahoo.com>
2025-12-26 21:07:30 -0800

    downgrade to 6.10 prisma

--- prisma.ts
+++ prisma.ts
@@ -1,14 +1,11 @@
 import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
 
 const connectionString = process.env.DATABASE_URL;
 if (!connectionString) {
   throw new Error("DB URL not set");
 }
 
-const adapter = new PrismaPg({ connectionString });
-
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 export { prisma };
